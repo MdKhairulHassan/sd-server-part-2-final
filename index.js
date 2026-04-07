f13a4b62be7c647933d57fd3b11a798351cb94ad
MdKhairulHassan <darknessmoon76@gmail.com>
2026-04-07 18:38:52 +0600

    envirment key set

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 3000;
 // ===============================================
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./smart-deals-firebase-admin-key.json');
+// const serviceAccount = require('./smart-deals-firebase-admin-key.json');
+// index.js
+const decoded = Buffer.from(
+  process.env.FIREBASE_SERVICE_KEY,
+  'base64',
+).toString('utf8');
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -210,7 +216,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!',
     );
